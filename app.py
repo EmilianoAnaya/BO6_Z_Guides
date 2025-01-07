f4f1d2220a3189cb89d70a7d92201873f97b6920
Gael Emiliano <gael.anaya@alumnos.udg.mx>
2025-01-07 13:43:10 -0600

    Reorganizing files and adapting them to Flask

--- app.py
+++ app.py
@@ -3,8 +3,12 @@ from flask import Flask, render_template, url_for, request, redirect
 app = Flask(__name__)
 
 @app.route("/")
-def get_home():
+def home():
     return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 
 if __name__ == "__main__":
