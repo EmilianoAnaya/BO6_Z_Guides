6aa6be63f9eb29d602410fbddf26809550b24f0e
Gael Emiliano <gael.anaya@alumnos.udg.mx>
2025-01-07 15:18:48 -0600

    Adding the 'About Me' page and implementing more Flask

--- app.py
+++ app.py
@@ -10,6 +10,10 @@ def home():
 def about():
     return render_template("about.html")
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return 'Page Not Found :(', 404
+
 
 if __name__ == "__main__":
     app.run(debug=True)
